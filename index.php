20aed2ab5f4ff0eb6e968de10dc2b7c55d57f766
sandhya12383 <sandhumsm15@gmail.com>
2020-12-03 16:12:56 +0530

    created new website

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Interio</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+<body class="bgcolor">
+    <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="gallery.html">Gallery</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact</a>
+          </li>
+          
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About Us</a>
+          </li>
+        </ul>
+        <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="Signup for free">
+          SignUp
+        </button>
+      </nav> 
+
+      <div class="container">
+          <div class="row">
+              <div class="col col-12 col-md-12">
+                <!-- <div class="col col-12 col-md-12">Welcome</div> -->
+                <center><div class="txtfont">Interio</div></center>
+                <center><div class="txtfont">Confidence begins at home</div></center>
+                <center><div class="txtfont">Creating lasting impressions through interior design.</div></center>
+
+                <div class="col col-12 col-sm-6 col-md-6">
+                   <!-- <center> <img src="images/Minimalist.jpeg" alt=""></center> -->
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12">
+                   <!-- <center> <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row no-gutters">
+                          <div class="col-md-4">
+                            <img src="images/interior.jpeg" class="card-img" alt="...">
+                          </div>
+                          <div class="col-md-8">
+                            <div class="card-body">
+                              <h5 class="card-title">Card title</h5>
+                              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                          </div>
+                        </div>
+                      </div></center> -->
+                </div>
+                 
+
+              </div>
+          </div>
+      </div>
+</body>
+</html>
